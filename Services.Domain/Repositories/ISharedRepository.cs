42cc51f340c0e4f41752dc4b5bb27873bc95310d
ayman-19 <152824182+ayman-19@users.noreply.github.com>
2024-12-14 23:29:58 +0200

    applychange

--- Services.Domain/Repositories/ISharedRepository.cs
+++ Services.Domain/Repositories/ISharedRepository.cs
@@ -9,10 +9,25 @@ namespace Services.Domain.Repositories
         Task<TSelctor> GetAsync<TSelctor>(
             Expression<Func<TEntity, bool>> predicate,
             Expression<Func<TEntity, TSelctor>> Selctor,
-            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes = null!,
             bool astracking = true,
             CancellationToken cancellationToken = default
         );
-        Task<bool> IsAnyExistAsync(Expression<Func<TEntity, bool>> pridecate);
+
+        Task<IReadOnlyCollection<TSelctor>> GetAllAsync<TSelctor>(
+            Expression<Func<TEntity, bool>> predicate,
+            Expression<Func<TEntity, TSelctor>> Selctor,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes = null!,
+            CancellationToken cancellationToken = default
+        );
+        Task DeleteAsync(TEntity entity);
+        Task<bool> IsAnyExistAsync(
+            Expression<Func<TEntity, bool>> pridecate,
+            CancellationToken cancellationToken = default
+        );
+        Task<int> CountAsync(
+            Expression<Func<TEntity, bool>> pridecate = null!,
+            CancellationToken cancellationToken = default
+        );
     }
 }
--- Services.Persistence/Repositories/SharedRepository.cs
+++ Services.Persistence/Repositories/SharedRepository.cs
@@ -18,10 +18,41 @@ namespace Services.Persistence.Repositories
             _entities = _context.Set<TEntity>();
         }
 
+        public async Task<int> CountAsync(
+            Expression<Func<TEntity, bool>> pridecate = null!,
+            CancellationToken cancellationToken = default
+        )
+        {
+            if (pridecate == null)
+                return await _entities.CountAsync(cancellationToken);
+
+            return await _entities.CountAsync(pridecate, cancellationToken);
+        }
+
+        public Task DeleteAsync(TEntity entity) => Task.Run(() => _entities.Remove(entity));
+
+        public async Task<IReadOnlyCollection<TSelctor>> GetAllAsync<TSelctor>(
+            Expression<Func<TEntity, bool>> predicate,
+            Expression<Func<TEntity, TSelctor>> Selctor,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes = null!,
+            CancellationToken cancellationToken = default
+        )
+        {
+            var query = _entities.AsQueryable().AsNoTracking();
+
+            if (predicate != null)
+                query = query.Where(predicate);
+
+            if (includes != null)
+                query = includes.Invoke(query);
+
+            return await query.Select(Selctor).ToListAsync(cancellationToken);
+        }
+
         public Task<TSelctor> GetAsync<TSelctor>(
             Expression<Func<TEntity, bool>> predicate,
             Expression<Func<TEntity, TSelctor>> Selctor,
-            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> includes = null!,
             bool astracking = true,
             CancellationToken cancellationToken = default
         )
@@ -40,7 +71,9 @@ namespace Services.Persistence.Repositories
             return query.Select(Selctor).FirstAsync(cancellationToken);
         }
 
-        public async Task<bool> IsAnyExistAsync(Expression<Func<TEntity, bool>> pridecate) =>
-            await _entities.AnyAsync(pridecate);
+        public async Task<bool> IsAnyExistAsync(
+            Expression<Func<TEntity, bool>> pridecate,
+            CancellationToken cancellationToken = default
+        ) => await _entities.AnyAsync(pridecate, cancellationToken);
     }
 }
