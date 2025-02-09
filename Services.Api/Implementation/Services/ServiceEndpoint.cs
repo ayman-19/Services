76e145f8a6a96da4d4984dc204e66c44d631c292
ayman-19 <152824182+ayman-19@users.noreply.github.com>
2025-02-09 16:50:44 +0200

    commit

---
+++ Services.Api/Implementation/Services/ServiceEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using Services.Api.Abstraction;
+
+namespace Services.Api.Implementation.Services
+{
+    public class ServiceEndpoint : IEndpoint
+    {
+        public void RegisterEndpoints(IEndpointRouteBuilder endpoints)
+        {
+            RouteGroupBuilder group = endpoints.MapGroup("/Services").WithTags("Services");
+            group.MapGet(
+                "Get",
+                () =>
+                {
+                    return Results.Ok("Services");
+                }
+            );
+        }
+    }
+}
