2a132eb0533cdce891954065fc4542187eade3f1
ayman-19 <152824182+ayman-19@users.noreply.github.com>
2025-02-14 17:20:27 +0200

    commit

---
+++ Services.Application/Features/Branchs/Comands/Update/Command.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Application.Features.Branchs.Comands.Update
+{
+	internal class Command
+	{
+	}
+}
