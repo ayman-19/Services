5b1c01020dccdb6754e0fedf8e271eba7f912069
ayman-19 <152824182+ayman-19@users.noreply.github.com>
2025-02-14 17:11:39 +0200

    commit

---
+++ Services.Domain/Abstraction/IBranchRepository.cs
@@ -0,0 +1,11 @@
+﻿using Services.Domain.Entities;
+using Services.Domain.Repositories;
+
+namespace Services.Domain.Abstraction
+{
+    public interface IBranchRepository : IRepository<Branch>
+    {
+        ValueTask<Branch> GetByIdAsync(Guid Id, CancellationToken cancellationToken);
+        ValueTask<Branch> GetByNameAsync(string Name, CancellationToken cancellationToken);
+    }
+}
---
+++ Services.Persistence/Repositories/BranchRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Services.Domain.Abstraction;
+using Services.Domain.Entities;
+using Services.Persistence.Data;
+
+namespace Services.Persistence.Repositories
+{
+    public sealed class BranchRepository : Repository<Branch>, IBranchRepository
+    {
+        private readonly ServiceDbContext _context;
+
+        public BranchRepository(ServiceDbContext context)
+            : base(context)
+        {
+            _context = context;
+        }
+
+        public async ValueTask<Branch> GetByIdAsync(Guid Id, CancellationToken cancellationToken) =>
+            await _context
+                .Set<Branch>()
+                .AsTracking()
+                .FirstAsync(id => id.Id == Id, cancellationToken);
+
+        public async ValueTask<Branch> GetByNameAsync(
+            string Name,
+            CancellationToken cancellationToken
+        ) =>
+            await _context
+                .Set<Branch>()
+                .AsTracking()
+                .FirstAsync(id => id.Name == Name, cancellationToken);
+    }
+}
