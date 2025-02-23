63b8fc14178318f162089558d51af14253ec250b
ayman-19 <152824182+ayman-19@users.noreply.github.com>
2025-02-23 10:49:11 +0200

    commit

---
+++ Services.Application/Features/Workers/Queries/GetAll/Handler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Application.Features.Workers.Queries.GetAll
+{
+    public sealed record GetAllWorkerHandler { }
+}
