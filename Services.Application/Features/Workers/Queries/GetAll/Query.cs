da669beba328e2a238f7f019b555714143268c1e
unknown <ayman-19>
2025-02-26 14:49:41 +0200

    commit

--- Services.Application/Features/Workers/Queries/GetAll/Query.cs
+++ Services.Application/Features/Workers/Queries/GetAll/Query.cs
@@ -3,6 +3,6 @@ using Services.Shared.Responses;
 
 namespace Services.Application.Features.Workers.Queries.GetAll
 {
-    public sealed record GetAllWorkerQuery
+    public sealed record GetAllWorkerQuery()
         : IRequest<ResponseOf<IReadOnlyCollection<GetAllWorkerResult>>>;
 }
