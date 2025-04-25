351f8b942d5780455d53b78a7d9574890e3394eb
unknown <ayman-19>
2025-04-25 15:49:43 +0300

    commit

---
+++ Services.Persistence/Repositories/RolePermissionRepository.cs
@@ -0,0 +1,18 @@
+﻿using Services.Domain.Abstraction;
+using Services.Domain.Models;
+using Services.Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Persistence.Repositories
+{
+    public class RolePermissionRepository : Repository<RolePermission>, IRolePermissionRepository
+    {
+        public RolePermissionRepository(ServiceDbContext context) : base(context)
+        {
+        }
+    }
+}
