96203805ca9aeeee6005c6c90450e9f043367c4a
ayman-19 <152824182+ayman-19@users.noreply.github.com>
2025-05-18 19:42:34 +0300

    commit

--- Services.Application/Features/Branchs/Comands/Create/Command.cs
+++ Services.Application/Features/Branchs/Comands/Create/Command.cs
@@ -7,11 +7,17 @@ namespace Services.Application.Features.Branchs.Comands.Create
     public sealed record CreateBranchCommand(
         string name,
         string description,
+        Guid userId,
         double langtuide,
         double latitude
     ) : IRequest<ResponseOf<CreateBranchResult>>
     {
         public static implicit operator Branch(CreateBranchCommand branchCommand) =>
-            new() { Langitude = branchCommand.langtuide, Latitude = branchCommand.latitude };
+            new()
+            {
+                Langitude = branchCommand.langtuide,
+                Latitude = branchCommand.latitude,
+                UserId = branchCommand.userId,
+            };
     }
 }
